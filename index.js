ab19f1a4a4e0994dd149d1b594e78a82b796ee94
Suphachai Sang <31473954+suphachai-sang@users.noreply.github.com>
2023-10-24 21:51:24 +0700

    ok

---
+++ index.js
@@ -0,0 +1,48 @@
+const line = require('@line/bot-sdk')
+const appress = require('express')
+const axios = require('axios').default
+const dotenv = require('dotenv')
+
+const env = dotenv.config().parsed
+const app = appress()
+
+const lineConfig = {
+    channelAccessToken: env.ACCESS_TOKEN,
+    channelSecret: env.SECRET_TOKEN
+}
+//create client
+//const client = new line.Client(lineConfig);
+
+const client = new line.messagingApi.MessagingApiClient({
+    channelAccessToken: env.ACCESS_TOKEN
+  });
+  
+
+app.post('/webhook', line.middleware(lineConfig), async (req, res) => {
+    Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result))
+    .catch((err) => {
+        console.error(err);
+        res.status(500).end();
+    });
+});
+
+function handleEvent(event) {
+    if (event.type !== 'message' || event.message.type !== 'text') {
+      return Promise.resolve(null);
+    }
+  
+    const echo = { type: 'text', text: event.message.text };
+  
+    return client.replyMessage({
+      replyToken: event.replyToken,
+      messages: [echo],
+    });
+  }
+  
+
+const port = env.PORT;
+app.listen(port, () => {
+    console.log(`listening on ${port}`);
+});
